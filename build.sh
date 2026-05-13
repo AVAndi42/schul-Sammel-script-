bdd416842cd17bf025df34e5b1b30416cdb28ad3
AVAndi42 <andi.feige@outlook.de>
2026-05-14 00:05:19 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Install libmagic system dependency
-apt-get update -qq && apt-get install -y -qq libmagic1 || true
-
-# Install Python dependencies
-pip install -r requirements.txt
